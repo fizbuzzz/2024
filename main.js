5deed7d8813efdf8a3d8a62658f428583c72bf20
Ivan Dud <ivandudukin@gmail.com>
2024-11-18 11:32:43 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,94 @@
+const _coordinatesApiKey = '46d57d99d13d421c9f11de6bdd53b047';
+const _weatherApiKey = 'c760d4fee5b94e0baf7112432241411';
+
+
+
+function getUserCoordinates () {
+    return new Promise((resolve,reject) => {
+        navigator.geolocation.getCurrentPosition((position) => {
+            const { latitude, longitude } = position.coords;
+            resolve({ latitude, longitude });
+        }, (err) => {
+            reject(err);
+        })
+    })
+    
+}
+
+async function getCity() {
+    const city = await getCoordinatesRequest();    
+    return  _filterData(city);
+}
+
+function _filterData (data) {
+    return {
+        city: data.results[0].components.city,
+        borough: data.results[0].components.borough,
+    }
+}
+
+
+
+async function getCoordinatesRequest (key = _coordinatesApiKey) {
+    try {
+        const {latitude, longitude} = await getUserCoordinates(); 
+        const response = await fetch(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}%2C${longitude}&key=${key}`);
+        if (!response.ok) {
+            throw new Error();
+        }
+      
+        const result =  await response.json();
+        return result;
+
+        
+    } catch (error) {
+        throw error;
+    }
+  
+}
+
+
+
+async function getTemperatureRequest(key = _weatherApiKey) {
+    try {
+        const {city, borough} = await getCity();
+        const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${key}&q=${city}&aqi=no`)
+        if (!response.ok) {
+            throw new Error();
+            
+        } 
+
+        const result =  await response.json();
+        result.borough = borough;
+        // console.log(result)
+
+        document.querySelector('.widget').innerHTML = `
+        <span class="widget__city">${result.location.name} , ${result.location.country}</span>
+        <span class="widget__temperature">${result.current.temp_c}&deg;C / ${result.current.temp_f}&deg;F</span>
+        <span class="widget__borough">${borough}</span>
+        
+        `
+
+       
+
+    } catch (error) {
+        if (error.message === 'Failed to fetch') {
+            document.querySelector('.widget').innerHTML = 'Something went wrong...'
+        } else {
+            document.querySelector('.widget').innerHTML = 'Please give access to your geolocation and reload the page'
+        }
+        console.log(error)
+        throw error;
+    }
+   
+}
+
+
+
+getTemperatureRequest()
+
+
+
+
+
+
